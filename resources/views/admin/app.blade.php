9f501ddc0f5135a77e38d02ba9a30b8e6e92333b
koojunho <koojh@9cells.com>
2016-03-12 18:19:02 +0900

    Admin 의 browser title 지정 가능하게 변경

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -2,7 +2,7 @@
     <html>
     <head>
         <meta charset="UTF-8">
-        <title>AdminLTE 2 | Dashboard</title>
+        <title>@yield('title')</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link href="{{ asset('/vendor/ninecells/assets-twbs3/bootstrap-3.3.6-dist/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
